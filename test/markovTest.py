087a24cfa07d08eeb373133262c17546bfcade16
cojaroge <cojaroge@ucsc.edu>
2016-12-14 18:24:01 -0800

    updated test file

--- test/markovTest.py
+++ test/markovTest.py
@@ -5,17 +5,6 @@ import nltk
 import re
 
 
-class POSifiedText(markovify.Text):
-	def word_split(self,sentence):
-		words = re.split(self.word_split_pattern,sentence)
-		words = [ "::".join(tag) for tag in nltk.pos_tag(words)]
-		print words
-		return words
-
-	def word_join(self, words):
-		sentence = " ".join(word.split("::")[0] for word in words)
-		return sentence
-
 with open(os.path.join(os.path.dirname('__file__'), "Paradise_Lost.txt")) as f:
 	p = f.read()
 
