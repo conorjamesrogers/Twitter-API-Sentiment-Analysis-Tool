7d7acdd4906fb9e2bc833d806808f744de8de26c
conorjamesrogers <conorjames.rogers@gmail.com>
2017-06-15 11:54:44 -0700

    python2.5 to python3...

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-ACCESS_TOKEN	= "xxxxxxxxxxxxxxxxxxxxxx"
-ACCESS_SECRET	= "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-CONSUMER_KEY	= "xxxxxxxxxxxxxxxxxxxxxxxxx"
-CONSUMER_SECRET	= "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+def access():
+    ACCESS_TOKEN= "xxxxxxxxxxxxxxxxxxxxxx"
+    ACCESS_SECRET= "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    CONSUMER_KEY= "xxxxxxxxxxxxxxxxxxxxxxxxx"
+    CONSUMER_SECRET= "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    return {access_token : ACCESS_TOKEN, access_secret : ACCESS_SECRET, consumer_key : CONSUMER_KEY, consumer_secret : CONSUMER_SECRET}
